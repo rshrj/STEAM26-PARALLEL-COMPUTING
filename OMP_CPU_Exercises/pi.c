20d1e2a1942d5851208f115d1bba9103ca8174dd
Rishi Raj <rishiraj.1012exp@gmail.com>
2026-08-10 11:01:21 +0200

    add vscode settings + pi exercise

--- OMP_CPU_Exercises/pi.c
+++ OMP_CPU_Exercises/pi.c
@@ -15,27 +15,46 @@ History: Written by Tim Mattson, 11/99.
 */
 #include <stdio.h>
 #include <omp.h>
+
+#define THREADS_WE_WANT 4
+
 static long num_steps = 100000000;
 double step;
 int main ()
 {
-	  int i;
-	  double x, pi, sum = 0.0;
+	  // int i;
+	  double pi, globalSum = 0.0;
 	  double start_time, run_time;
 
 	  step = 1.0/(double) num_steps;
 
-        	 
+
 	  start_time = omp_get_wtime();
+		int nthreads;
+		double threadSum[THREADS_WE_WANT];
 
-	  for (i=1;i<= num_steps; i++){
-		  x = (i-0.5)*step;
-		  sum = sum + 4.0/(1.0+x*x);
-	  }
+		omp_set_num_threads(THREADS_WE_WANT);
+		#pragma omp parallel
+		{
+			int idthread = omp_get_thread_num();
+			nthreads = omp_get_num_threads();
+			double sum = 0.0;
 
-	  pi = step * sum;
+			for (int i = idthread; i < num_steps; i = i + nthreads) {
+				double x = (i-0.5)*step;
+				sum = sum + 4.0/(1.0+x*x);
+			}
+
+			threadSum[idthread] = sum;
+		}
+
+		for (int i = 0; i < nthreads; ++i) {
+			globalSum = globalSum + threadSum[i];
+		}
+
+	  pi = step * globalSum;
 	  run_time = omp_get_wtime() - start_time;
-	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
+	  printf("\n pi with %ld steps is %lf in %lf seconds, using %d threads\n", num_steps, pi, run_time, nthreads);
 }	  
 
 
